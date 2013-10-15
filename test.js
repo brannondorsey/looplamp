03c9b301937cd2ebb17572f18ab93debe5410dcb
Brannon Dorsey <brannon@brannondorsey.com>
2013-10-15 15:41:16 +0000

    Deleted old test.js files

--- test.js
+++
@@ -1,9 +0,0 @@
-var Pixel = require('./classes/adafruit_pixel').Pixel;
-
-var lights = Pixel('/dev/spidev0.1', 11);
-console.log(lights);
-lights.all(0xff, 0, 0);
-lights.sync(); // Updates strand with current buffer
-lights.set(1, 0xff, 0xff, 0);
-lights.set(2, 0xff, 0xff, 0xff);
-lights.sync();
